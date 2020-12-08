7bf80c5fb155d2de504e9762cc1a179259c169c0
David AZOULAY <dazoulay@simplicite.fr>
2020-12-08 12:24:20 +0100

    Format

--- src/com/simplicite/extobjects/DemoAPIs/DemoAPI2.java
+++ src/com/simplicite/extobjects/DemoAPIs/DemoAPI2.java
@@ -6,30 +6,38 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.simplicite.util.ObjectDB;
-import com.simplicite.util.tools.Parameters;
 import com.simplicite.util.exceptions.HTTPException;
 import com.simplicite.util.exceptions.SearchException;
+import com.simplicite.util.tools.Parameters;
 
 public class DemoAPI2 extends com.simplicite.webapp.services.RESTServiceExternalObject {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public Object get(Parameters params) throws HTTPException
 	{
 		ObjectDB obj = borrowAPIObject("DemoProduct"); // borrow an API object instance from the pool
 		try {
 			List<String> parts = params.getURIParts(getName());
+
 			if (parts.size() == 0) { // search
 				List<String[]> rows = obj.getTool().search(params.getParameters()); // filtered search
+
+				// Remove row IDs
 				JSONArray list = new JSONArray(obj.toJSON(rows, null, false, true, true));
 				for (int i = 0; i < list.length(); i++)
 					list.getJSONObject(i).remove("row_id");
+
 				return list;
 			} else { // get (/<reference>)
 				String reference = parts.get(0);
-				List<String[]> rows = obj.getTool().search(new JSONObject().put("demoPrdReference", reference));
+				List<String[]> rows = obj.getTool().search(new JSONObject().put("demoPrdReference", reference)); // filtered search using reference only
+
+				// No unique record matching reference => not found
 				if (rows.size() != 1)
 					return notFound("No prodduct for reference " + reference);
-				JSONObject item = new JSONObject(obj.toJSON(rows.get(0), null, false, true));
-				return item.remove("row_id");
+
+				return new JSONObject(obj.toJSON(rows.get(0), null, false, true)).remove("row_id");
 			}
 		} catch (SearchException e) {
 			return error(e);
