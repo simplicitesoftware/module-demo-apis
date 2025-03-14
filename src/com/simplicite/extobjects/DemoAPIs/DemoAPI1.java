5c5cde75bda783762f7c7a6890e66f1e1c67b003
designer (Designer) <simplicite@simplicite.fr>
2025-03-14 11:01:06 +0100

    Removed useless code

--- src/com/simplicite/extobjects/DemoAPIs/DemoAPI1.java
+++ src/com/simplicite/extobjects/DemoAPIs/DemoAPI1.java
@@ -15,14 +15,12 @@ public class DemoAPI1 extends com.simplicite.webapp.services.RESTMappedObjectsEx
 	private static final String SUPPLIERS = "suppliers";
 	private static final String PRODUCTS = "products";
 	private static final String ORDERS = "orders";
-	private static final String STATS = "stats";
 
 	@Override
 	public void init(Parameters params) {
 		setOpenAPISpec(JSONTool.OPENAPI_OAS2);
 		setOpenAPIDesc("This is a **simplified** variant of the demo API for the following business objects:\n\n- Suppliers\n- Products\n- Orders");
 		setOpenAPIVers("v1");
-		//addOperationDesc(null, OPERATION_PING, "This is the **ping** operation");
 
 		addObject(SUPPLIERS, "DemoSupplier", "Supplier");
 		addField(SUPPLIERS, "code", "demoSupCode");
@@ -36,28 +34,24 @@ public class DemoAPI1 extends com.simplicite.webapp.services.RESTMappedObjectsEx
 		addField(PRODUCTS, "type", "demoPrdType", "Product type", null);
 		addField(PRODUCTS, "name", "demoPrdName", "Product name", null);
 		addField(PRODUCTS, "available", "demoPrdAvailable", "Available product?", null);
+		//addOperationDesc(PRODUCTS, OPERATION_SEARCH, "This is the **search** operation for the _product_ business object");
+		//addOperationDesc(PRODUCTS, OPERATION_GET, "This is the **get** operation for the _product_ business object");
+		//addOperationDesc(PRODUCTS, OPERATION_CREATE, "This is the **create** operation for the _product_ business object");
+		//addOperationDesc(PRODUCTS, OPERATION_UPDATE, "This is the **update** operation for the _product_ business object");
+		//addOperationDesc(PRODUCTS, OPERATION_DELETE, "This is the **delete** operation for the _product_ business object");
 
-		addObject(ORDERS, "DemoOrder", "Order");
-		// or to force pagination (10 by 10): addObject(ORDERS, "DemoOrder", "Order", true, 10);
-		addRefField(ORDERS, PRODUCTS, "productId", "demoOrdPrdId", "productOrders", EMBED_LINKS, "Reference to product's row ID");
-		addField(ORDERS, "number", "demoOrdNumber");
-		addField(ORDERS, "date", "demoOrdDate");
-		addField(ORDERS, "status", "demoOrdStatus");
-		addField(ORDERS, "productReference", "demoOrdPrdId.demoPrdReference");
-		addField(ORDERS, "productName", "demoOrdPrdId.demoPrdName");
-		addField(ORDERS, "productType", "demoOrdPrdId.demoPrdType");
-		addField(ORDERS, "productSupplierCode", "demoOrdPrdId.demoPrdSupId.demoSupCode");
-		addField(ORDERS, "productSupplierName", "demoOrdPrdId.demoPrdSupId.demoSupName");
-		//addOperationDesc("products", OPERATION_SEARCH, "This is the **search** operation for the _product_ business object");
-		//addOperationDesc("products", OPERATION_GET, "This is the **get** operation for the _product_ business object");
-		//addOperationDesc("products", OPERATION_CREATE, "This is the **create** operation for the _product_ business object");
-		//addOperationDesc("products", OPERATION_UPDATE, "This is the **update** operation for the _product_ business object");
-		//addOperationDesc("products", OPERATION_DELETE, "This is the **delete** operation for the _product_ business object");
-
-		addObject(STATS, "DemoStats", DESC_HIDDEN_FROM_SCHEMA);
-		addField(STATS, "status", "demoOrdStatus");
-		addField(STATS, "count", "demoStsCount");
-		addField(STATS, "quantity", "demoStsQuantity");
-		addField(STATS, "total", "demoStsTotal");
+		if (getGrant().accessObject("DemoOrder")) {
+			addObject(ORDERS, "DemoOrder", "Order");
+			// or to force pagination (10 by 10): addObject(ORDERS, "DemoOrder", "Order", true, 10);
+			addRefField(ORDERS, PRODUCTS, "productId", "demoOrdPrdId", "productOrders", EMBED_LINKS, "Reference to product's row ID");
+			addField(ORDERS, "number", "demoOrdNumber");
+			addField(ORDERS, "date", "demoOrdDate");
+			addField(ORDERS, "status", "demoOrdStatus");
+			addField(ORDERS, "productReference", "demoOrdPrdId.demoPrdReference");
+			addField(ORDERS, "productName", "demoOrdPrdId.demoPrdName");
+			addField(ORDERS, "productType", "demoOrdPrdId.demoPrdType");
+			addField(ORDERS, "productSupplierCode", "demoOrdPrdId.demoPrdSupId.demoSupCode");
+			addField(ORDERS, "productSupplierName", "demoOrdPrdId.demoPrdSupId.demoSupName");
+		}
 	}
 }
